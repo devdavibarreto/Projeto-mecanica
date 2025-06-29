22e5ca8576968c446e3f1b7be0a61beb84460a59
devdavibarreto <davibarretojkda@gmail.com>
2025-06-29 14:03:24 -0300

    Atualização no método dos elevadores

--- src/com/mecanica/oficina/Elevadores.java
+++ src/com/mecanica/oficina/Elevadores.java
@@ -7,28 +7,41 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Elevadores extends Elevador {
 
     public static boolean elevador1(boolean oculpados) {
+        int numeroSorteado = elevadorOculpado();
         boolean oculpado = oculpados;
         return oculpado;
     }
 
-    public boolean elevador2() {
-        boolean oculpado = elevadorOculpado();
+    public static boolean elevador2(boolean oculpados) {
+        boolean oculpado = oculpados;
         return oculpado;
     }
-    public boolean elevador3() {
-        boolean oculpado = elevadorOculpado();
+    public static boolean elevador3(boolean oculpados) {
+        boolean oculpado = oculpados;
         return oculpado;
     }
 
 
 // fazer para cada número sorteado um elevador ser selecionado
 
-    public  static boolean elevadorOculpado() {
-        int numeroSorteado = ThreadLocalRandom.current().nextInt(0, 2);
+    public  static int elevadorOculpado() {
+        int numeroSorteado = ThreadLocalRandom.current().nextInt(0, 4);
         boolean oculpados = false;
         if (numeroSorteado == 1) {
             oculpados = true;
             elevador1(oculpados);
+        } else if (numeroSorteado == 2) {
+            oculpados = true;
+            elevador2(oculpados);
+        }else if (numeroSorteado == 3) {
+            oculpados = true;
+            elevador3(oculpados);
+        }
+        else {
+            oculpados = true;
+            elevador1(oculpados);
+            elevador2(oculpados);
+            elevador3(oculpados);
         }
         return oculpados;
 
