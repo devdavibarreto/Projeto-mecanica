93a20b7f5431b24a3ec5b1273c4d5a0521721699
devdavibarreto <davibarretojkda@gmail.com>
2025-06-29 17:24:24 -0300

    Criação da class fornecedor

--- src/com/mecanica/fornecedores/Fornecedores.java
+++ src/com/mecanica/fornecedores/Fornecedores.java
@@ -3,8 +3,7 @@ package com.mecanica.fornecedores;
 import com.mecanica.model.Fornecedor;
 
 public class Fornecedores extends Fornecedor {
-    private String nome;
-    private String matricula;
+
 
 
 }
--- src/com/mecanica/model/Fornecedor.java
+++ src/com/mecanica/model/Fornecedor.java
@@ -1,5 +1,22 @@
 package com.mecanica.model;
 
 public class Fornecedor  {
+    private String nome;
+    private String matricula;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
